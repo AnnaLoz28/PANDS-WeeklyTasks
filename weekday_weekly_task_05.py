74c31c63a901b7f0da910abd2c71aa165834d7e3
Anna Lozenko <annaloz28695@gmail.com>
2025-03-02 21:50:44 +0000

    task 5 improved

--- weekday_weekly_task_05.py
+++ weekday_weekly_task_05.py
@@ -5,7 +5,7 @@
 import datetime  # python doesn't have a "date" data type, we need to import the "datetime" module to work with dates as date objects
 
 current_date = datetime.datetime.now()  #determine the current date (year, month, day, hour, minute, second, microsecond)
-weekday = current_date.strftime("%A")  # print the weekday
+weekday = current_date.strftime("%A")  # determine the weekday
 
 if weekday != "Sunday" and "Saturday":
     print("Yes, unfortunately today is a weekday.")
