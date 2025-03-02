feda483d92868e6d8fd23014e871af16e81ae1c7
Anna Lozenko <annaloz28695@gmail.com>
2025-03-02 21:46:54 +0000

    weekly task 5

---
+++ weekday_weekly_task_05.py
@@ -0,0 +1,14 @@
+# weekday.py
+# program that outputs whether or not today is a weekday. 
+# author: Anna Lozenko
+
+import datetime  # python doesn't have a "date" data type, we need to import the "datetime" module to work with dates as date objects
+
+current_date = datetime.datetime.now()  #determine the current date (year, month, day, hour, minute, second, microsecond)
+weekday = current_date.strftime("%A")  # print the weekday
+
+if weekday != "Sunday" and "Saturday":
+    print("Yes, unfortunately today is a weekday.")
+else:
+    print("It is weekend, yay!")
+
